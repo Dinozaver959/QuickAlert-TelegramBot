e5cf359924368ddcb82e635b22f2172a04a4bb22
Dinozaver959 <david.grilec@gmail.com>
2023-09-13 17:42:39 +0400

    stable domain

--- testScript.ts
+++ testScript.ts
@@ -3,7 +3,7 @@ import {updateDestionationWebhook} from "./quickalerts";
 
 async function main(){
 
-    await updateDestionationWebhook("wallet-tracking-test-2", "https://f53a-87-200-88-192.ngrok-free.app/webhook");
+    await updateDestionationWebhook("wallet-tracking-test-2", "https://funky-hippo-gently.ngrok-free.app/webhook");
 
 }
 
